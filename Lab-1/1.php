27c44d5ec90e5de5f0a1b793463b96de392fe806
Jadeja Rudrarajsinh <140775968+Rudrarajsinh-9@users.noreply.github.com>
2024-12-17 21:59:52 +0530

    Create 1.php

---
+++ Lab-1/1.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title> Lab 1 A1 </title>
+</head>
+<body>
+	<?php
+	$x=10;
+	$y=20;
+	if($x%2==0)
+	{
+		echo " $x even";
+	}
+	else
+	{
+		echo " $x odd";
+	}
+?>
+</body>
+</html>
