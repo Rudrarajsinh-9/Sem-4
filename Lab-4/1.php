0f20643c63bbccfe3bdcac4664a992848b44371e
Jadeja Rudrarajsinh <140775968+Rudrarajsinh-9@users.noreply.github.com>
2024-12-17 22:04:14 +0530

    Create 1.php

---
+++ Lab-4/1.php
@@ -0,0 +1,17 @@
+<?php
+$a = array(10,20,30,40);
+for($i=0;$i<4;$i++)
+{
+    echo"$i";
+}
+echo"</hr>";
+
+foreach($a as $b=>$m)
+{
+    echo"$a";
+}
+var_dump ($a);
+echo("$a");
+print_r($a);
+$menu = array("pizza"=>45);
+?>
