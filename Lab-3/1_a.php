e7cc9cdcf30d8baeadbb303f50c0223b11fab28c
Jadeja Rudrarajsinh <140775968+Rudrarajsinh-9@users.noreply.github.com>
2024-12-17 22:02:54 +0530

    Create 1_a.php

---
+++ Lab-3/1_a.php
@@ -0,0 +1,18 @@
+	<html>
+		<head>
+			<title> Lab_3_1</title>
+		</head>
+		<body>
+			<?php
+				$n = 5;
+				for($i=1; $i<=5; $i++)
+				{
+					echo "</br>";
+					for($j=1; $j<=$i; $j++)
+					{
+						print " *";
+					}
+				}
+			?>
+		</body>
+	</html>
